8cbee85cf4ab98d55614ad0ac8b9d202e855d732
Glenn Craver <gtcaccs@gmail.com>
2011-10-02 21:38:54 -0700

    Login Part is a chunk of view for login/out forms Makepost is a chunk of view for adding posts.

---
+++ application/views/loginpart.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+	<?PHP
+
+		$session = Session::instance();
+		$sesdat = $session->as_array();
+		//display login form
+		$session = Session::instance();
+		$sesdat = $session->as_array();
+		if(isset($sesdat['loggedin']))
+		{
+			echo "<form action=\"http://dev.vm/blog/auth/logout\" method=\"post\">";
+			echo Form::hidden('hidden', 'logout');
+			echo $sesdat['username']." | ".Form::submit('Logout', 'Logout');
+			echo Form::close();
+		}
+		else
+		{
+			echo "<div><form action=\"http://dev.vm/blog/auth/\" method=\"post\"></div>";
+			echo Form::input('userfield', 'username');
+			echo Form::password('passfield', 'password');
+			echo Form::submit('req_type', 'Login');
+			echo Form::submit('req_type', 'Register');
+			echo Form::close();
+		}
+		
+	?>
+	</body>
+</html>
---
+++ application/views/makepost.php
@@ -0,0 +1,112 @@
+<html>
+	<head>
+	<style type ="text/css">
+			/*General Styling*/
+			body
+			{	font-family	:Georgia;
+			}
+			h1
+			{	font-style	:italic;
+			}
+			p 
+			{	
+				font-size	:18px;
+				text-align	:left;
+			}
+			/*IDs*/
+			/*Classes*/
+			.title .content .login
+			{
+				font-family :Georgia;
+			}
+			.login
+			{
+				position	:relative;
+				font-size	:12px;
+				text-align	:right;
+			}
+			
+			.home
+			{
+				position	:relative;
+				font-size	:12px;
+				text-align :center;
+			}
+			
+			.title
+			{
+				position	:relative;
+				font-size	:30px;
+				text-align	:left;
+			}
+			.content
+			{
+				position	:relative;
+				font-size	:20px;
+				text-align	:left;
+				top			:-50px;
+			}
+			.delete
+			{	position	:relative;
+				font-size	:10px;
+				font-family	:Arial;
+				text-align	:right;
+				top			:+20px;
+			}
+			
+			/*Divisions*/
+			div.par
+			{	
+				position 	:relative;
+				padding		:1%;
+				border		:10px;
+				margin 		:2%;
+			}
+			div.ex
+			{	
+				position 	:relative;
+				width		:30%;
+				left		:35%;
+				padding		:1%;
+				border		:10px;
+				margin 		:2%;
+			}
+			
+			div.danger
+			{	
+				position 	:relative;
+				width		:30%;
+				left		:48%;
+				padding		:1%;
+				border		:10px 0xFF0000;
+				margin 		:2%;
+			}
+		</style>
+	<title>Create Post</title>
+	</head>
+	
+	<body>
+	
+		<p class="home">
+			<a href="http://dev.vm/blog/">HOME</a>
+		</p>
+		
+		<?PHP
+			$sesdat = Session::instance()->as_array();
+			
+			echo '<div class="ex">';
+			echo "<div><form action=\"http://dev.vm/blog/editpost/create\" method=\"post\"></div>"; 
+			echo Form::hidden('hidden', 'add_post');
+			echo Form::hidden('author', $sesdat['username']);
+			echo '<div>Title: '.Form::input('title', '').'</div>';
+			echo '<div>Author: '.$sesdat['username'].'</div>';
+			echo '<div>'.Form::textarea('content').'</div>';
+			echo Form::submit('submit', 'Add Post');
+			echo Form::close();
+			echo '</div>';
+		?>
+		
+	</body>
+
+
+</html>
